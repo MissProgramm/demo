7ed931ebe01d45cd86f44ab00fa8fe51f232ed1c
Um e Habiba <missprogrammer27@gmail.com>
2024-08-15 10:46:59 +0500

    just push this folder

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  Text,
+  View,StyleSheet,
+} from 'react-native';
+import Header from './Header';
+
+
+
+const App=()=>
+{
+ 
+  return (
+<View>
+<Header></Header>
+  <Text style={{fontSize:30,marginLeft:60}}>React native App! </Text>
+<Text style={styles.linee}>let's start it . </Text>
+</View>
+  )  
+};
+
+const styles=StyleSheet.create({
+  linee:{
+backgroundColor:'green',
+borderRadius:15,
+
+borderColor:'red',
+borderWidth:3,
+height:50,
+fontSize:30,
+
+  }}
+)
+
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+function Header()
+{
+    return(
+<View>
+    <Text >
+        This is Header Component !
+    </Text>
+</View>
+    )
+};
+
+export default Header;
