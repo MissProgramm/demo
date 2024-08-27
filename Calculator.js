067d3e1f97e6962fa529b4b81b2bc8b9a7824f93
Um e Habiba <missprogrammer27@gmail.com>
2024-08-27 23:39:21 +0500

    starting modifications

--- Calculator.js
+++ Calculator.js
@@ -1,44 +1,49 @@
 import { StyleSheet, Text, TextInput,TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Calculator = () => {
+
+const [display,setDisplay]=useState("")
+
   return (
-    <View style={{flex:1}}>
+    <View >
       <Text>programmer's Calculator</Text>
       <TextInput  placeholder='text' style={styles.txt}></TextInput>
 
 
 <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>c</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>0</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>.</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>/</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn}  onPress={()=>{setDisplay("")}}>  <Text style={styles.btntxt} >c</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn}  onPress={()=>{setDisplay(display+"0")}}>   <Text style={styles.btntxt}>0</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn}    onPress={()=>{setDisplay(display+".")}}>  <Text style={styles.btntxt}>.</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn}    onPress={()=>{setDisplay(display+"/")}}> <Text style={styles.btntxt}>/</Text></TouchableOpacity>
 
 </View>
 <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>7</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>8</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>9</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>*</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"7")}}><Text style={styles.btntxt}>7</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"8")}}><Text style={styles.btntxt}>8</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"9")}}><Text style={styles.btntxt}>9</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"*")}}><Text style={styles.btntxt}>*</Text></TouchableOpacity>
 
 </View>
 <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>4</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>5</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>6</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>-</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+ "4")}}><Text style={styles.btntxt}>4</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+ "5")}}><Text style={styles.btntxt}>5</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+ "6")}}><Text style={styles.btntxt}>6</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+  "-")}}><Text style={styles.btntxt}>-</Text></TouchableOpacity>
 
 </View>
 <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>1</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>2</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>3</Text></TouchableOpacity>
-  <TouchableOpacity style={styles.btn}><Text style={styles.btntxt}>+</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"1")}}><Text style={styles.btntxt}>1</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"2")}}><Text style={styles.btntxt}>2</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"3")}}><Text style={styles.btntxt}>3</Text></TouchableOpacity>
+  <TouchableOpacity style={styles.btn} onPress={()=>{setDisplay(display+"+")}}><Text style={styles.btntxt}>+</Text></TouchableOpacity>
 
 </View>
+
+
 
 <View style={{flexDirection:'row' }}>
-<TouchableOpacity style={[styles.btn , {borderRadius:23}]}><Text style={styles.btntxt}>=</Text></TouchableOpacity>
+<TouchableOpacity style={[styles.btn , {borderRadius:23}]} ><Text style={styles.btntxt}>=</Text></TouchableOpacity>
 </View>
 
 
