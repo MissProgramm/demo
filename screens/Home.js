8cad8ebee930915aba694c25631c352bba155e04
Um e Habiba <missprogrammer27@gmail.com>
2024-09-12 13:40:12 +0500

    implement stack navigation on two screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{flex: 1, backgroundColor: 'royalblue', margin: 10, padding: 15}}>
+      <Text>This is home screen .</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Notification');
+        }}
+        title="Go to notification page "
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- screens/Notification.js
+++ screens/Notification.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 const Notification = () => {
+  const navigation = useNavigation();
   return (
     <View>
-      <Text>Notification</Text>
+      <Text>This is Notification Screen.</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate('Home');
+        }}
+        title="go to home screen "
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
